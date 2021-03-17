0b26501fbd6ad58194ee5175746b72e8f5df9cbc
Web time <70425331+WebTimeGit@users.noreply.github.com>
2021-03-17 22:06:26 +0200

    add slider swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // PRELOADER ACTIVATED
-    setTimeout(()=> {
+    setTimeout(() => {
         let page_preloader = document.querySelector('#page_preloader');
         if (!page_preloader.classList.contains('done')) {
             page_preloader.classList.add('done')
         }
-    }, 700);
+    }, 1000);
 
 
+    // STICKY SELECTOR .header_menuBar
+    let header = document.querySelector('#header');
+    let main = document.querySelector('.main');
+    let main_homePage = document.querySelector('.main_homePage');
+    let headerHeight = header.offsetHeight;
+    let menuBarHeight = document.querySelector('.header_menuBar').offsetHeight;
+    let scrollToSticky = headerHeight - menuBarHeight;
+
+    if (!main_homePage) {
+        main.style.padding = `${headerHeight}px 0 0 0`;
+    } else {
+        main.style.padding = `0 0 0 0`;
+    }
+
+    function addSticky() {
+        if (window.pageYOffset >= scrollToSticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+
+    window.onscroll = function () {
+        addSticky()
+    };
 
 
-    //OPEN HEADER SEARCH MODAL
-
+    //INIT HEADER SEARCH MODAL
     let headerSearchBtn = document.querySelector('.header_search-btn');
     let headerSearchModal = document.querySelector('.search_modalBlock');
     let searchModalClose = document.querySelector('.search_modalBlock-close');
 
     if (headerSearchBtn) {
-        headerSearchBtn.addEventListener('click', ()=> {
+        headerSearchBtn.addEventListener('click', () => {
             headerSearchModal.classList.add('open');
-            document.body.style.overflow='hidden';
+            document.body.style.overflow = 'hidden';
         })
-        searchModalClose.addEventListener('click', ()=> {
+        searchModalClose.addEventListener('click', () => {
             headerSearchModal.classList.remove('open');
-            document.body.style.overflow='initial';
+            document.body.style.overflow = 'initial';
+
         })
     }
+
+
+    // MAIN SWIPER BANNER
+    new Swiper('.main-banner', {
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
 
 });
